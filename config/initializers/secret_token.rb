1b7701ceb52c8f6bc437f7df7b368ce01b36809e
Ruthan <rcfreese@gmail.com>
2013-08-16 15:18:50 -0400

    Initial commit. pg 0.12.2 not working.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemoApp::Application.config.secret_token = '3d696d8bce605b071462d842c5a0c7dc91744b42a00cd6ff9ff929e47a8edbad8e63a5dd2e0ba083cd912b57b5d6975eea01fb52f2e2ef505aadc39ecd5debbc'
